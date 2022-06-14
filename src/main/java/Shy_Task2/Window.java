91c18d2ba5cbbad74955e4c21f0bab6d0fe1e3f8
Yehuda1223 <103877785+Yehuda1223@users.noreply.github.com>
2022-06-14 15:15:21 +0300

    עדכון
    Signed-off-by: Yehuda1223 <103877785+Yehuda1223@users.noreply.github.com>

---
+++ src/main/java/Shy_Task2/Window.java
@@ -0,0 +1,42 @@
+package Shy_Task2;
+
+import javax.swing.*;
+
+import static Shy_Task2.MyRobt.Whatsapp;
+
+public class Window extends JFrame {
+    public static final int WINDOW_HIGHT = 500;
+    public static final int WINDOW_WITGH = 500;
+    int c;
+
+    public static void main(String[] args) throws InterruptedException {
+        Window();
+    }
+
+    public static void Window() {
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+        frame.setLayout(null);
+        frame.setResizable(false);
+        frame.setBounds(0, 0, 500, 500);
+
+
+        JButton button = new JButton("click here");
+        button.setBounds(200, 200, 100, 100);
+        frame.add(button);
+        button.addActionListener((event) -> {
+
+            try {
+                Whatsapp();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+
+        });
+
+    }
+
+}
